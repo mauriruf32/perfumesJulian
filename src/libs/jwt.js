40be62bd883d9aeba32573e76cb7d3f74cc5fac6
Mauricio <mauriruffini32@gmail.com>
2025-05-02 19:39:00 -0300

    pruba url del cliente4

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,12 +1,8 @@
 import { TOKEN_SECRET } from "../config.js";
 import jwt from "jsonwebtoken";
 
-if (!TOKEN_SECRET) {
-  throw new Error("TOKEN_SECRET is not defined in config.js");
-}
-
-export function createAccessToken(payload) {
-    return new Promise((resolve, reject) => {
+export function createAccessToken (payload) {
+    return new Promise((resolve, reject) =>{
         jwt.sign(
             payload,
             TOKEN_SECRET,
@@ -20,22 +16,3 @@ export function createAccessToken(payload) {
         );
     });
 }
-
-// import { TOKEN_SECRET } from "../config.js";
-// import jwt from "jsonwebtoken";
-
-// export function createAccessToken (payload) {
-//     return new Promise((resolve, reject) =>{
-//         jwt.sign(
-//             payload,
-//             TOKEN_SECRET,
-//             {
-//                 expiresIn: "1d",
-//             },
-//             (err, token) => {
-//                 if (err) reject(err);
-//                 resolve(token);
-//             }
-//         );
-//     });
-// }
