01dec073276fd2b3ca8b09062390009666f5c7b8
Mauricio <mauriruffini32@gmail.com>
2025-05-16 19:45:01 -0300

    login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -18,8 +18,8 @@ function LoginPage() {
   }, [isAuthenticated])
 
   return (
-    <div class="flex h-[calc(100vh-100px)] items-center justify-center  dark:border-gray-600 dark:bg-gray-900 dark:text-white">
-      <div class="bg-blue-800  max-w-md w-full p-10 rounded-md" >
+    <div class="flex h-[calc(100vh-100px)] items-center justify-center  dark:border-gray-600 dark:bg-gray-900 dark:text-gray-900">
+      <div class="bg-gray-800 text-white max-w-md w-full p-10 rounded-md dark:border-gray-600 dark:bg-white dark:text-gray-900" >
       {
         SignInErrors.map((error, i) => (
           <div class="bg-red-500 p-2 text-white text-center my-2">
@@ -31,7 +31,7 @@ function LoginPage() {
       <form  onSubmit={onSubmit} >
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+      <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2 dark:text-gray-900" for="grid-last-name">
         Email
       </label>
       <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" 
@@ -44,7 +44,7 @@ function LoginPage() {
   </div>
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+      <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2 dark:text-gray-900" for="grid-password">
         Password
       </label>
       <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" 
