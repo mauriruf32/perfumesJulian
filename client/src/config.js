51540463fe9fc620fe9fa8ab15ca7d6b33cd3be2
Mauricio <mauriruffini32@gmail.com>
2025-06-27 20:37:20 -0300

    connect2

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-// export const URL = "https://perfumes-julian.vercel.app/api";
-export const URL = 'http://localhost:4000/api';
+export const URL = "https://perfumes-julian.vercel.app/api";
+// export const URL = 'http://localhost:4000/api';
 
 
 
