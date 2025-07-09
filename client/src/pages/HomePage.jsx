6d0f81ad56b11d21633b017c03aa248ee485f329
Mauricio <mauriruffini32@gmail.com>
2025-07-09 17:59:57 -0300

    homepage cols

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ function HomePage() {
     <div>
       <CarouselDefault/>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 p-6">
-        <div className="grid grid-cols-2 min-[500px]:grid-cols-3 min-[768px]:grid-cols-4 min-[1024px]:grid-cols-5 min-[1280px]:grid-cols-6 gap-3 sm:gap-4 md:gap-5">
+        <div className="grid grid-cols-3 min-[500px]:grid-cols-3 min-[768px]:grid-cols-4 min-[1024px]:grid-cols-5 min-[1280px]:grid-cols-6 gap-3 sm:gap-4 md:gap-5">
           {products.map((product) => (
             <ProductCard product={product} key={product.id} />
           ))}
