66fabbd3f586b8628954fbd695fec0cdd5dac5dd
Mauricio <mauriruffini32@gmail.com>
2025-07-16 19:34:47 -0300

    homepage front

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import { useProducts } from '../context/ProductsContext';
 import ProductCard from '../components/ProductCard';
 import { CarouselDefault } from '../components/Carousel';
@@ -7,24 +7,25 @@ function HomePage() {
   const { getProducts, products } = useProducts();
 
   useEffect(() => {
-    getProducts()
-  }, [])
+    getProducts();
+  }, []);
 
   return (
     <div>
-      <CarouselDefault/>
+      <CarouselDefault />
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 p-6">
-        <div className="grid grid-cols-3 min-[500px]:grid-cols-3 min-[768px]:grid-cols-4 min-[1024px]:grid-cols-5 min-[1280px]:grid-cols-6 gap-3 sm:gap-4 md:gap-5">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {products.map((product) => (
             <ProductCard product={product} key={product.id} />
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default HomePage;
+
 
 // import { useEffect } from 'react'
 // import { useProducts } from '../context/ProductsContext';
@@ -41,14 +42,16 @@ export default HomePage;
 //   return (
 //     <div>
 //       <CarouselDefault/>
-//           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-6 justify-self-center dark:border-gray-600 dark:bg-gray-900 dark:text-white">
-//       {products.map((product) => (
-//         <ProductCard product={product} key={product.id} />
-//       ))}
+//       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 p-6">
+//         <div className="grid grid-cols-3 min-[500px]:grid-cols-2 min-[768px]:grid-cols-2 min-[1024px]:grid-cols-5 min-[1280px]:grid-cols-6 gap-3 sm:gap-4 md:gap-5">
+//           {products.map((product) => (
+//             <ProductCard product={product} key={product.id} />
+//           ))}
+//         </div>
+//       </div>
 //     </div>
-//     </div>
-
 //   )
 // }
 
 // export default HomePage;
+
